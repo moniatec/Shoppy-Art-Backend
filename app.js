19152b97c9799404e009df62783d9b40a26d2345
monia <moniatec@gmail.com>
2020-05-10 16:32:08 -0400

    finishing set up backend

--- app.js
+++ app.js
@@ -1,30 +1,45 @@
 const express = require("express");
 const morgan = require("morgan");
-const { environment } = require('./config');
+const cors = require("cors");
+const { ValidationError } = require("sequelize");
+const { environment } = require("./config");
+
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000" }));
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the express-sequelize-starter!");
-});
+
 
 // Catch unhandled requests and forward to error handler.
 app.use((req, res, next) => {
   const err = new Error("The requested resource couldn't be found.");
+  err.errors = ["The requested resource couldn't be found."];
   err.status = 404;
   next(err);
 });
 
-// Custom error handlers.
+// Error handlers. (must have all four arguments to communicate to Express that
+// this is an error-handling middleware function)
 
-// Generic error handler.
+// Process sequelize errors
+app.use((err, req, res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = "Sequelize Error";
+  }
+  next(err);
+});
+
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
   const isProduction = environment === "production";
   res.json({
     title: err.title || "Server Error",
     message: err.message,
+    errors: err.errors,
     stack: isProduction ? null : err.stack,
   });
 });
